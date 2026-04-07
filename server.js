d87a92701ffe00c01ea49f684265dde5f37b6d48
Johan MALET <johan.mallet@ecogies.fr>
2026-04-07 02:59:34 +0200

    fix: limite requêtes JSON augmentée à 50mb pour upload factures

--- server.js
+++ server.js
@@ -50,7 +50,8 @@ const initDataFolder = async () => {
 initDataFolder();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(express.static('public'));
 
